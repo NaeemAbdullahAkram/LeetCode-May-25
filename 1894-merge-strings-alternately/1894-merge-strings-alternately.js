16462973d7ea55b32245fa89d8be5f27502204e3
Naeem Akram <153123217+NaeemAbdullahAkram@users.noreply.github.com>
2025-05-15 10:37:30 +0500

    Time: 52 ms (11.42%), Space: 53.9 MB (54.95%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.js
@@ -0,0 +1,12 @@
+
+var mergeAlternately = function(word1, word2) {
+    var maxLength = Math.max(word1.length, word2.length);
+    var chars = [];
+    for (var i = 0; i < maxLength; i++) {
+        if (word1[i])
+            chars.push(word1[i]);
+        if (word2[i])
+            chars.push(word2[i]);
+    }
+    return chars.join('');
+};
