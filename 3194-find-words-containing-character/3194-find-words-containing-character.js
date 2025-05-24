7dfb780b6542eb0e125b37aa6aec0a98625c5d88
Naeem Akram <153123217+NaeemAbdullahAkram@users.noreply.github.com>
2025-05-24 22:53:27 +0500

    Time: 1 ms (82.59%), Space: 57.7 MB (63.83%) - LeetHub

---
+++ 3194-find-words-containing-character/3194-find-words-containing-character.js
@@ -0,0 +1,10 @@
+
+var findWordsContaining = function(words, x) {
+    let res = [];
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].includes(x)) {
+            res.push(i);
+        }
+    }
+    return res;
+};
